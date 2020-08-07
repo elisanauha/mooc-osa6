1a8869332c83ed36987d188aa569233465f515e1
elisanauha <elisa.nauha@gmail.com>
2020-08-07 09:22:21 +0300

    Do 6.13 initial anecdotes from json-server

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,11 +1,11 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-]
+// const anecdotesAtStart = [
+//   'If it hurts, do it more often',
+//   'Adding manpower to a late software project makes it later!',
+//   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+//   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+//   'Premature optimization is the root of all evil.',
+//   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+// ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -17,9 +17,9 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+// const initialState = anecdotesAtStart.map(asObject)
 
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   console.log('anecdote state now: ', state)
   console.log('anecdote action', action)
 
@@ -36,6 +36,8 @@ const anecdoteReducer = (state = initialState, action) => {
         .sort((a, b) => b.votes - a.votes)
     case 'NEW_ANECDOTE':
       return [...state, asObject(action.data.anecdote)]
+    case 'INIT_ANECDOTES':
+      return action.data.anecdotes
     default:
       return state
   }
@@ -52,6 +54,13 @@ export const voteForAnecdote = (anecdote) => {
   return {
     type: 'VOTE',
     data: { id: anecdote.id, content: anecdote.content },
+  }
+}
+
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: { anecdotes },
   }
 }
 
